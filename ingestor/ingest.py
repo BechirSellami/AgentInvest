7540929ebc983c3d739e171f861e8dd26413c624
Bechir Sellami <bechirsellami@Bechirs-MacBook-Pro.local>
2025-06-22 21:59:28 +0400

    removed unnecessary files

--- ingestor/ingest.py
+++ ingestor/ingest.py
@@ -5,9 +5,13 @@ from yfinance import Ticker
 from dotenv import load_dotenv
 from azure.identity import DefaultAzureCredential
 from azure.search.documents import SearchClient
+import logging
+# Set up logging   
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Load data from csv and Yahoo Finance
-df = pd.read_csv("../data/sample_companies.csv")
+df = pd.read_csv("./data/sample_companies.csv")
 
 # Clean tickers
 df['ticker'] = df['ticker'].apply(lambda x : x.split(":")[1])
@@ -60,6 +64,21 @@ def ingest_to_search(docs, index_name="companies-index"):
     endpoint = os.environ["AZURE_SEARCH_SERVICE"]
     credential = DefaultAzureCredential()
     client = SearchClient(endpoint=endpoint, index_name=index_name, credential=credential)
+    
+    # Assert we can get Azure Credential
+    try:
+        token = credential.get_token("https://search.azure.com/.default")
+        logger.info("Token acquired for:", token)
+    except Exception as e:
+        logger.error(f"Failed to acquire Azure credential: {e}")
+
+    # Assert we can connect to Azure Search Service
+    try:
+        index_client.get_index(args.index_name)
+        logger.info("Successfully connected to Azure Search Service")
+    except Exception as e:
+        logger.error(f"Failed to connect to Azure Search Service: {e}")
+    
     if not docs:
         print("No documents to upload")
         return []
